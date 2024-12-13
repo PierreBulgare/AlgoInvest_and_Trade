e86f02cf66d715432ab3cbc5cfc4fe0b97df66fa
Pierre BULGARE <pierre.bulgare@gmail.com>
2024-12-13 22:41:03 +0100

    Gestion des trois fichiers CSV

--- main.py
+++ main.py
@@ -4,10 +4,7 @@ import algorithms.optimized as optimized_algo
 from algorithms.common import get_best_investment
 
 def main():
-    file_path = "dataset2.csv"
-    output_file_path_bruteforce = "datas/best_combinaison_bruteforce.csv"
-    output_file_path_optimized = "datas/best_combinaison_optimized.csv"
-
+    files = ["csv/liste_actions.csv", "csv/dataset_1.csv", "csv/dataset_2.csv"]
     while True:
         algo_choice = input(
                 "===================================\n"
@@ -21,18 +18,21 @@ def main():
         if algo_choice.isdigit():
             algo_choice = int(algo_choice)
             if algo_choice in [1, 2, 3]:
+                for file in files:
+                    output_file_path_bruteforce = f"datas/best_combinaison_bruteforce_{file.split("/")[1].strip(".csv")}.csv"
+                    output_file_path_optimized = f"datas/best_combinaison_optimized_{file.split("/")[1].strip(".csv")}.csv"
                     if algo_choice == 1:
                         # Calcul du temps d'exécution de bruteforce.py
                         print("Exécution de bruteforce.py ...")
                         start_time = time.time()
-                        get_best_investment(file_path, output_file_path_bruteforce, bruteforce_algo.find_best_investment)
+                        get_best_investment(file, output_file_path_bruteforce, bruteforce_algo.find_best_investment)
                         bruteforce_execution_duration = time.time() - start_time
                         print(f"Bruteforce Algorithm Duration : {bruteforce_execution_duration:.3f} secondes")
                     elif algo_choice == 2:
                         # Calcul du temps d'exécution de optimized.py
                         print("Exécution de optimized.py ...")
                         start_time = time.time()
-                        get_best_investment(file_path, output_file_path_optimized, optimized_algo.find_best_investment)
+                        get_best_investment(file, output_file_path_optimized, optimized_algo.find_best_investment)
                         optimized_execution_duration = time.time() - start_time
                         print(f"Optimized Algorithm Duration : {optimized_execution_duration:.3f} secondes")
                     else:
